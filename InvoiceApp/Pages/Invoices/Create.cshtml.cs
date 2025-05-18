3eeb7fe8f630e3bbf7c9572e4fcc8fc1ce64617b
minhsang082525 <sanglm252508@gmail.com>
2025-05-18 14:07:18 +0700

    Add project files.

---
+++ InvoiceApp/Pages/Invoices/Create.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using InvoiceApp.Models;
+using InvoiceApp.Services;
+
+namespace InvoiceApp.Pages.Invoices
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ApplicationDbContext context;
+
+        [BindProperty]
+        public InvoiceDto InvoiceDto { get; set; } = new();
+
+
+        public CreateModel(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void OnGet()
+        {
+        }
+
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var invoice = new Invoice
+            {
+                Number = InvoiceDto.Number,
+                Status = InvoiceDto.Status,
+                IssueDate = InvoiceDto.IssueDate,
+                DueDate = InvoiceDto.DueDate,
+
+                Service = InvoiceDto.Service,
+                UnitPrice = InvoiceDto.UnitPrice,
+                Quantity = InvoiceDto.Quantity,
+
+                ClientName = InvoiceDto.ClientName,
+                Email = InvoiceDto.Email,
+                Phone = InvoiceDto.Phone,
+                Address = InvoiceDto.Address
+            };
+
+            context.Invoices.Add(invoice);
+            context.SaveChanges();
+
+            return RedirectToPage("Index");
+        }
+    }
+}
